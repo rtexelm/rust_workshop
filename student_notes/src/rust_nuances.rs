73ed731ec6be0f30d13f451decc5f3786573cba2
rtexelm <texelist@gmail.com>
2023-06-28 13:50:51 -0400

    Learning expressions and borrowing in Rust

--- student_notes/src/rust_nuances.rs
+++ student_notes/src/rust_nuances.rs
@@ -1,13 +1,42 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
 // ownership, borrowing, lifetimes, references, borrow checker, and pointers
 
 fn bf1(s: &String) {
+    println!("{s}"); // new pointer because of reference, the &
+}
+
+fn bf2(s: String) {
     println!("{s}");
 }
 
 fn borrow_func() {
     let string1 = "Hello".to_string();
+    bf1(&string1);
+    println!("=================================");
+    bf2(string1.clone());
+}
+
+// fn move_func() {
+//     let x: i32 = 1;
+//     let y: i32 = x;
+//     println!("{x}, {y}")
+// }
+
+fn move_func() {
+    let str1: String = "Hello".to_string();
+    let str2: String = str1.clone();
+    println!("{str1}, {str2}")
+}
+
+fn expr_func() {
+    let x = 1;
+
+    // loop is like a "while true" loop}
 }
 
 fn main() {
     borrow_func();
+    move_func();
 }
